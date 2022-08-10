2634a72172c4c1cf3e2b54fcfe7b12dc52570100
Dyanna Azevedo <dyvazevedo@gmail.com>
2022-08-10 20:56:58 -0300

    adicionando out

---
+++ out/_next/static/chunks/pages/home-1d3214fdebfb9609.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[229],{8862:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home",function(){return u(7614)}])}},function(n){n.O(0,[614,774,888,179],(function(){return _=8862,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
