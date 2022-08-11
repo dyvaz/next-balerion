a45841fb8e68b073ee9dfd4df1f2de005180c946
Dyanna Azevedo <dyvazevedo@gmail.com>
2022-08-11 17:26:36 -0300

    Refatorando o index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import HomePage from "./home";
-import Login from "../pages/auth/login";
 
 function Index() {
-  let isSignedIn = true;
-  return <>{isSignedIn ? <HomePage /> : <Login />}</>;
-  // return <HomePage />;
+  return <HomePage />;
 }
 export default Index;
